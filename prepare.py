7ae208304bdedfb1eb56c76bfbd9c59a6e34852f
lam061197 <sonlam06111998@gmail.com>
2022-10-14 16:09:43 +0700

    up

--- prepare.py
+++ prepare.py
@@ -12,8 +12,8 @@ import nltk
 py_vncorenlp.download_model(save_dir=cwd+'/vncorenlp')
 nltk.download('punkt')
 
-os.system('wget https://huggingface.co/vinai/phobert-base/raw/main/vocab.txt')
-os.system('wget https://raw.githubusercontent.com/chaoswithinyou/PreSumm/master/src/others/added_vocab.txt')
+# os.system('wget https://huggingface.co/vinai/phobert-base/raw/main/vocab.txt')
+# os.system('wget https://raw.githubusercontent.com/chaoswithinyou/PreSumm/master/src/others/added_vocab.txt')
 
 id = "17XJo6d-yg4VDjttKCxuZY_Yrc3THF1Iz"
 gdown.download(id=id, quiet=False)
