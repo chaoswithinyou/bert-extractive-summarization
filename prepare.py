0e9b08a42c7147c005d49ee73842809239e7c2fc
lam061197 <sonlam06111998@gmail.com>
2022-10-14 16:10:21 +0700

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -7,7 +7,7 @@ install_req = 'pip3 install -r '+cwd+'/req.txt'
 os.system(install_req)
 
 import py_vncorenlp
-import gdown
+# import gdown
 import nltk
 py_vncorenlp.download_model(save_dir=cwd+'/vncorenlp')
 nltk.download('punkt')
@@ -15,5 +15,5 @@ nltk.download('punkt')
 # os.system('wget https://huggingface.co/vinai/phobert-base/raw/main/vocab.txt')
 # os.system('wget https://raw.githubusercontent.com/chaoswithinyou/PreSumm/master/src/others/added_vocab.txt')
 
-id = "17XJo6d-yg4VDjttKCxuZY_Yrc3THF1Iz"
-gdown.download(id=id, quiet=False)
+# id = "17XJo6d-yg4VDjttKCxuZY_Yrc3THF1Iz"
+# gdown.download(id=id, quiet=False)
